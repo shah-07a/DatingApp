d187a9b9cc1a328b5891a4774f0a7d5f347debaf
RM Shah <jindeshahmandar@gmail.com>
2024-09-21 11:44:08 +0530

    End of extension methods 21-09-24

---
+++ API/Extensions/AppServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class AppServiceExtensions
+    {
+        //=== An Extension method ===
+        public static IServiceCollection AddAppServices(this IServiceCollection services, IConfiguration config) 
+        {
+            services.AddControllers();
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlServer(config.GetConnectionString("APIConnString"));
+            });
+
+            services.AddCors(); //=== Cross Domain Setting
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,18 @@
-using API.Data;
-using API.Interfaces;
-using API.Services;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.Extensions.Options;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("APIConnString"));
-});
+builder.Services.AddAppServices(builder.Configuration); //=== Added an extension method
+builder.Services.AddIdentityServices(builder.Configuration); //=== Added an extension method
 
-builder.Services.AddCors(); //=== Cross Domain Setting
-builder.Services.AddScoped<ITokenService, TokenService>();  
 var app = builder.Build();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://loacalhost:4200")); //=== Cross Domain Setting
+
+app.UseAuthentication();    //=== Keep Authentication on the top
+app.UseAuthorization();
 
 app.MapControllers();
 
